4d947652cfb1522e699a00929d260635e57aabd5
Monika1014 <monikasr1014@gmail.com>
2025-07-18 23:11:15 +0530

    Update 26. Adder 2.v

--- 26. Adder 2.v
+++ 26. Adder 2.v
@@ -5,7 +5,7 @@ module top_module (
 );
     wire cout;
     add16 (a[15:0], b[15:0], 1'b0, sum[15:0], cout);
-    add16 (a[31:16], b[31:16], cout, sum[31:16], );
+    add16 (a[31:16], b[31:16], cout, sum[31:16]);
 endmodule
 
 module add1 ( input a, input b, input cin, output sum, output cout );
